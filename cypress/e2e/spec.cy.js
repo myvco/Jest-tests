b435ccf48cfcf9c7993322a7aa4192b8502e098d
myvco <manon.audibert@ynov.com>
2026-02-19 14:56:13 +0100

    test cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('home page spec', () => {
   it('deployed react app to localhost', () => {
     cy.visit('http://localhost:5173');
-    cy.contains('Registration Form');
+    cy.contains('Welcome');
   })
 })
