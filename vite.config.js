71715407946757a13a4100e3478e07e720185f25
myvco <manon.audibert@ynov.com>
2026-02-20 18:49:00 +0100

    Update config

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    base: '/Jest-tests/',
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: ['./setupTests.js'],
-        coverage: {
-            provider: 'v8',
-            reporter: ['text', 'html', 'lcov'],
-            reportsDirectory: './coverage'
-        }
+  plugins: [react()],
+  base: '/Jest-tests/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./setupTests.js'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      reportsDirectory: './coverage',
+      include: [
+        'src/utils/**/*.js',
+        'src/component/**/*.jsx'
+      ],
+      exclude: [
+        'src/main.jsx',
+        'src/App.jsx',
+        'src/component/Counter.jsx'
+      ]
     }
+  }
 })
